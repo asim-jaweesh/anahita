405b59b2cda22620564553bc677ef65994d902a1
asim-jaweesh <asim.abdo_20@hotmail.com>
2014-03-17 17:53:13 +0300

    prevent reflected XSS in session return parameter
    encode the session parameter to prevent reflected cross site scripting
    (XSS)

--- src/components/people/site/controllers/session.php
+++ src/components/people/site/controllers/session.php
@@ -88,7 +88,9 @@ class ComPeopleControllerSession extends ComBaseControllerResource
     	$person = $this->getService('repos://site/people.person')->find(array('userId'=>JFactory::getUser()->id));
     	$this->_state->setItem($person);
     	if ( isset($_SESSION['return']) ) {
-    	    $this->_state->append(array('return'=>$_SESSION['return']));
+		$validatedSessionID = htmlspecialchars(strip_tags(trim($_SESSION['return'])));
+		$validatedSessionID
+    	    $this->_state->append(array('return'=>$validatedSessionID));
     	}
     	return $person;
     }
